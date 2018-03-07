a586d17d9f4ad2bc98866497bec94a43ad319cf9
kratika-saxena-01 <36702553+kratika-saxena-01@users.noreply.github.com>
2018-03-07 22:07:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,6 +69,12 @@ var template=`
 
 app.use(morgan('combined'));
 
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
