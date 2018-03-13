38287c1e349f49b6313fb602dfb941974479b779
kratika-saxena-01 <36702553+kratika-saxena-01@users.noreply.github.com>
2018-03-13 22:02:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,3 +28,24 @@ button.onclick=function(){
     
     
 };
+
+//submit name
+var inputName = document.getElementById('name');
+var name= inputName.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+  //make a request to the server and send the name
+  //capture a name of list and render it as a list
+  var names=['name1','name2','name3','name4'];
+  var list='';
+  for(var i=0;i<names.length;i++)
+  {
+      list + = '<li>' + names[i] + '</li>';
+  }
+  
+  var ul=document.getElementById('namelist');
+  ul.innerHTML=list;
+  
+  
+  
+};
