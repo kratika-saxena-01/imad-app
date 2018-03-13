62c30c7731053aa4302fbfca201d8f3a3deef277
kratika-saxena-01 <36702553+kratika-saxena-01@users.noreply.github.com>
2018-03-13 19:22:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //change the text of the one div
 var element = document.getElementById('one');
 element.innerHTML='kratika';
+
+//move image
+var img=document.getElementById('two');
+img.onClick=function(){
+  img.style.marginLeft='100px';  
+};
