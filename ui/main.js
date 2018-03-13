a067f4cea3607c3b700c6e3b21fb69743cf7f84f
kratika-saxena-01 <36702553+kratika-saxena-01@users.noreply.github.com>
2018-03-13 19:33:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ element.innerHTML='kratika';
 
 //move image
 var img=document.getElementById('two');
+var marginLeft=0;
+function moveRight(){
+  marginLeft=marginLeft+10;
+  img.style.marginLeft=marginLeft + 'px';
+}
 img.onclick=function(){
-  img.style.marginLeft='100px';  
+   
+  var interval =setInterval(moveRight,100);
 };
